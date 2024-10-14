8e27b2b7b65f908d911e3c93daa04acd87f8d726
Kirill <syskirill.com>
2024-10-14 14:47:04 +0300

    Initial Commit

---
+++ SepBill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SepBill
+//
+//  Created by Кирилл Сысоев on 14.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
